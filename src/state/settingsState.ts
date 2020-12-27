c6ac5655be5db3572787c6ddc76cd11b072722eb
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-12-27 22:15:00 +0100

    chore: make starter simpler

--- src/state/settingsState.ts
+++
@@ -1,8 +0,0 @@
-import { State } from "@simple-html/core";
-
-type formDetails = { loggedin: boolean; username: string };
-
-export const formState = new State<formDetails>(
-  "FORM_STATE",
-  {} as formDetails
-);
